17cda96f12ec0fd4590da324608487d674ab5512
Stefan Alberts <stefan6573@gmail.com>
2017-02-28 17:07:41 +0200

    [script] Minor changes to launch script.

--- src/scripts/shimmer.sh
+++ src/scripts/shimmer.sh
@@ -1,34 +1,56 @@
 #!/bin/bash
 
+LONGOPTS="sdl,sdl2"
+
 SHIMMER_LIBS="/usr/local/libshimmer"
-APP=
-SHIM=
+SHIM_SDL="$SHIMMER_LIBS/libshimmer_sdl.so"
+SHIM_SDL2="$SHIMMER_LIBS/libshimmer_sdl2.so"
 
-for ARG in $@
-do
-    BIN=$(type -p $ARG )
-
-    if [ -f "$BIN" ]
-    then
-        echo "shimmer.sh: Found $ARG at $BIN"
-        LDD=$(ldd $BIN)
-
-        SDL=$(echo $LDD | grep libSDL-1.2.so | wc -l)
-        SDL2=$(echo $LDD | grep libSDL2.so | wc -l)
-
-        if [[ $SDL == 1 ]]
-        then
-            APP=$ARG
-            SHIM="$SHIMMER_LIBS/libshimmer_sdl.so"
-            break
-        elif [[ $SDL2 == 1 ]]
-        then
-            APP=$ARG
-            SHIM="$SHIMMER_LIBS/libshimmer_sdl2.so"
-            break
-        fi
-    fi
+args=$(getopt -l $LONGOPTS -- "$@")
+eval set -- "$args"
+while [ $# -ge 1 ]; do
+        case "$1" in
+                --)
+                    shift
+                    break
+                   ;;
+                --sdl)
+                        SHIM="$SHIM_SDL"
+                        shift
+                        ;;
+                -sdl2)
+                        SHIM="$SHIM_SDL2"
+                        shift
+                        ;;
+        esac
+        shift
 done
+
+if [ -n "$SHIM" ]
+then
+    for ARG in $@
+    do
+        BIN=$(type -p $ARG )
+
+        if [ -f "$BIN" ]
+        then
+            echo "shimmer.sh: Found $ARG at $BIN"
+            LDD=$(ldd $BIN)
+
+            if [[ $(echo $LDD | grep libSDL-1.2.so | wc -l) == 1 ]]
+            then
+                APP=$ARG
+                SHIM="$SHIMMER_LIBS/libshimmer_sdl.so"
+                break
+            elif [[ $(echo $LDD | grep libSDL2.so | wc -l) == 1 ]]
+            then
+                APP=$ARG
+                SHIM="$SHIMMER_LIBS/libshimmer_sdl2.so"
+                break
+            fi
+        fi
+    done
+fi
 
 if [ -n "$SHIM" ]
 then
